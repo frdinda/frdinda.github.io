9ab1259a48f3e9e09cdabe1404ab3c515af5d8dc
aruan1965 <frdinda.47@gmail.com>
2020-06-10 14:41:52 +0700

    latihan

--- latihan/latihan_portofolio_3/index.js
+++ latihan/latihan_portofolio_3/index.js
@@ -8,17 +8,17 @@ btn_profile.addEventListener("click", function(){
     filter.style.zIndex="0";
     filter.style.opacity=".8";
     filter.style.transition="opacity 1s";
-    profile.style.transform="translate(0vw)";
+    profile.style.transform="translate(0%)";
     profile.style.transition="all 1.7s";
-    home.style.transform="translate(150vw)";
+    home.style.transform="translate(150%)";
     home.style.transitionDelay="2s";
 });
 
 btn_home.addEventListener("click", function(){
-    profile.style.transform="translate(150vw)";
+    profile.style.transform="translate(150%)";
     profile.style.transition="all 1.7s";
     profile.style.transitionDelay=".2s";
-    home.style.transform="translate(0vw)";
+    home.style.transform="translate(0%)";
     home.style.transitionDelay="0s";
     filter.style.zIndex="-999";
     filter.style.opacity="0";
