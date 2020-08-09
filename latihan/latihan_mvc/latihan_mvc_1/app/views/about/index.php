58e7c9efa3d5ccb49d869734d085702798afb327
frdinda <frdinda.47@gmail.com>
2020-08-09 15:32:25 +0700

    ubah

--- latihan/latihan_mvc/latihan_mvc_1/app/views/about/index.php
+++
@@ -1,9 +0,0 @@
-<br>
-<br>
-<div class="container">
-    <div class="jumbotron">
-        <h1 class="display-4">INI PAGE ABOUT</h1>
-        <br>
-        <img src="<?= BASEURL;?>/img/cat.jpg" alt="Kucing" width="390">
-    </div>
-</div>    
--- latihan/latihan_mvc/latihan_mvc_2/app/views/templates/footer.php
+++
@@ -1,5 +0,0 @@
-    <script src="<?= BASEURL;?>/js/jquery.js"></script>
-    <script src="<?= BASEURL;?>/js/bootstrap.js"></script>
-    <script src="<?= BASEURL;?>/js/script.js"></script>
-</body>
-</html>
