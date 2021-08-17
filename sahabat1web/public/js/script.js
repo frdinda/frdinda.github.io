6f5e5f4136e56b60d210947faab50280ccd2f26d
frdinda <frdinda.47@gmail.com>
2021-08-17 15:51:37 +0700

    lol

--- sahabat1web/public/js/script.js
+++ sahabat1web/public/js/script.js
@@ -5,12 +5,15 @@
 function changeBg() {
     var navbar = document.getElementById('navbar');
     var scrollValue = window.scrollY;
+    var windowWidth = window.innerWidth;
+    console.log(scrollValue);
     if (scrollValue < 211) {
         navbar.classList.remove('bgColor');
         document.getElementById('navbar').style.transitionDuration = '1s';
     } else {
         navbar.classList.add('bgColor');
     }
+    
 }
 
 window.addEventListener('scroll', changeBg);
